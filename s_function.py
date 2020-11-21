6849bd7988bc439e8fafc0cfe1c5ebbb2e9fe09b
Tratut <trataty111@gmail.com>
2020-11-21 18:44:05 +0300

    1 версия Фунций: sa, sb, ss

---
+++ s_function.py
@@ -0,0 +1,21 @@
+def ft_len(st):
+    kol = 0
+    for i in st:
+        kol += 1
+    return kol
+
+
+def sa(mass_a):
+    if ft_len(mass_a) > 1:
+        mass_a[0], mass_a[1] = mass_a[1], mass_a[0]
+        return mass_a
+
+
+def sb(mass_b):
+    if ft_len(mass_b) > 1:
+        mass_b[0], mass_b[1] = mass_b[1], mass_b[0]
+        return mass_b
+
+
+def ss(mass_a, mass_b):
+    return sa(mass_a), sb(mass_b)
