d7690502daefd64c1b72688aba723ac2673d867f
Tratut <trataty111@gmail.com>
2020-11-21 19:28:06 +0300

    adsgsdghsd

---
+++ r_function_part_1.py
@@ -0,0 +1,27 @@
+def ft_len(mass):
+    i = 0
+    for g in mass:
+        i += 1
+    return i
+
+
+def ra(mass_a):
+    x = mass_a[0]
+    mass_cop = []
+    for i in range(1, ft_len(mass_a)):
+        mass_cop.append(mass_a[i])
+    mass_cop.append(x)
+    return mass_cop
+
+
+def rb(mass_b):
+    x = mass_b[0]
+    mass_cop = []
+    for i in range(1, ft_len(mass_b)):
+        mass_cop.append(mass_b[i])
+    mass_cop.append(x)
+    return mass_cop
+
+
+def rr(mass_a, mass_b):
+    return ra(mass_a), rb(mass_b)
--- test.py
+++ test.py
@@ -20,4 +20,5 @@ def sb(mass_b):
 def ss(mass_a, mass_b):
     return sa(mass_a), sb(mass_b)
 
+
 print(121342)
