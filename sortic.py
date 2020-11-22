b8aa3fcd086ed8c29d261c906c47473e10067a1e
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-22 20:51:16 +0300

    Add files via upload

--- sortic.py
+++ sortic.py
@@ -5,10 +5,12 @@ from additional_function import ft_find_lst, ft_len, ft_min, ft_slice
 mass_a = []
 mass_b = []
 
-number = input()
-while number != '!':
-    mass_a.append(int(number))
-    number = input()
+file = open('input.txt', mode='r', encoding='UTF-8')
+lines = file.readlines()
+for line in lines:
+    for char in line:
+        if char != ' ':
+            mass_a.append(int(char))
 
 iterator = 1
 
@@ -20,7 +22,7 @@ while mass_a:
 
     # если первый элемент - минимум, то убераем его в другой стэк и сортируем дальше
     if ft_min(mass_a) == mass_a[0]:
-        print('COMMAND: pb')
+        print("COMMAND" + "\033[31m {}" .format("pb") + "\033[0m")
         mass_b.append(mass_a[0])
         mass_a = ft_slice(mass_a, 1, ft_len(mass_a))
         print(f'MASSIVE_A: {mass_a}')
@@ -31,13 +33,13 @@ while mass_a:
         # списка до концов списка и находим до куда ближе
         if minimal_index >= ft_len(mass_a) - minimal_index - 1:
             mass_a = rra(mass_a)
-            print('COMMAND rra')
+            print("COMMAND" + "\033[31m {}".format("rra") + "\033[0m")
             print(f'MASSIVE_A: {mass_a}')
             print(f'MASSIVE_B: {mass_b}')
             print('=' * ft_len(f'MASSIVE_B: {mass_b}'))
         elif minimal_index <= ft_len(mass_a) - minimal_index - 1:
             mass_a = ra(mass_a)
-            print('COMMAND ra')
+            print("COMMAND" + "\033[31m {}" .format("ra") + "\033[0m")
             print(f'MASSIVE_A: {mass_a}')
             print(f'MASSIVE_B: {mass_b}')
             print('=' * ft_len(f'MASSIVE_B: {mass_b}'))
@@ -46,4 +48,11 @@ while mass_a:
 
 mass_a = mass_b
 
-print(mass_a)
+result_string = ''
+for number in mass_a:
+    result_string += str(number) + ' '
+
+
+file = open('output.txt', mode='w')
+file.write(result_string)
+file.close()
