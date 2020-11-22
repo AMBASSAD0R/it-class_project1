e7f3235e917584edf3d47d5805fe949013a3ff29
Tratut <trataty111@gmail.com>
2020-11-22 18:20:09 +0300

    Добавили в return название функции

--- rra_rrb_rrr.py
+++ rra_rrb_rrr.py
@@ -8,7 +8,7 @@ def rra(mass_a):
     res_x = []
     for i in range(ft_len(res) - 1):
         res_x.append(res[i])
-    return res_x
+    return res_x, "rra"
 
 
 def rrb(mass_b):
@@ -18,9 +18,9 @@ def rrb(mass_b):
     res_x = []
     for i in range(ft_len(res) - 1):
         res_x.append(res[i])
-    return res_x
+    return res_x, "rrb"
 
 
 def rrr(mass_a, mass_b):
     """одновременно выполнить rra и rrb"""
-    return rra(mass_a), rrb(mass_b)
+    return rra(mass_a), rrb(mass_b), "rrr"
--- sa_sb_ss.py
+++ sa_sb_ss.py
@@ -5,16 +5,16 @@ def sa(mass_a):
     """Меняет местами первые 2 элемента на вершине стека a"""
     if ft_len(mass_a) > 1:
         mass_a[0], mass_a[1] = mass_a[1], mass_a[0]
-        return mass_a
+        return mass_a, "sa"
 
 
 def sb(mass_b):
     """Меняет местами первые 2 элемента на вершине стека b"""
     if ft_len(mass_b) > 1:
         mass_b[0], mass_b[1] = mass_b[1], mass_b[0]
-        return mass_b
+        return mass_b, "sb"
 
 
 def ss(mass_a, mass_b):
     """sa и sb одновременно"""
-    return sa(mass_a), sb(mass_b)
+    return sa(mass_a), sb(mass_b), "ss"
