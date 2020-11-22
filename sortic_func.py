e2997979aecc9383eae253f8e53783324578542b
Tratut <trataty111@gmail.com>
2020-11-22 20:02:07 +0300

    Изменен алгоритм check_sortic.py Создан sortic_func.py

--- sortic_func.py
+++ sortic_func.py
@@ -1,6 +1,6 @@
 from rra_rrb_rrr import *
 from ra_rb_rr import *
-from additional_function import ft_find_lst, ft_len, ft_min
+from additional_function import ft_find_lst, ft_len, ft_min, ft_slice
 
 
 def sortic(mass_a):
@@ -16,7 +16,7 @@ def sortic(mass_a):
         if ft_min(mass_a) == mass_a[0]:
             mass_commands.append('pb')
             mass_b.append(mass_a[0])
-            mass_a = mass_a[1:]
+            mass_a = ft_slice(mass_a, 1, ft_len(mass_a))
         else:
             # находим рассстояние от концов
             # списка до концов списка и находим до куда ближе
