0254944c9e4c3ef60e8d1d6b6dac76716b56b885
Tratut <trataty111@gmail.com>
2020-11-25 21:40:44 +0300

    Принты вынесены в отдельную функцию

--- sortic.py
+++ sortic.py
@@ -2,8 +2,34 @@ from rra_rrb_rrr import *
 from ra_rb_rr import *
 from additional_function import ft_find_lst, ft_len, ft_min, ft_slice
 
+
+# функция, которая выводит визуализацию процесса сортировки в output.txt
+
+
+def txt_output(fname, command):
+    fname.write(f'COMMAND {command}' + '\n')
+    fname.write(f'MASSIVE_A: {mass_a}' + '\n')
+    fname.write(f'MASSIVE_B: {mass_b}' + '\n')
+    fname.write('=' * ft_len(f'MASSIVE_B: {mass_b}') + '\n')
+
+
+# функция, которая выводит визуализацию процесса сортировки в консоль
+
+def console_output(command):
+    print('COMMAND' + '\033[31m {}'.format(command) + '\033[0m')
+    print(f'MASSIVE_A: {mass_a}')
+    print(f'MASSIVE_B: {mass_b}')
+    print('=' * ft_len(f'MASSIVE_B: {mass_b}'))
+
+
 mass_a = []
 mass_b = []
+
+# Ввод в одну строку
+# numbers = input()
+# for i in range(0, ft_len(numbers), 2):
+#     mass_a.append(int(numbers[i]))
+
 
 file = open('input.txt', mode='r', encoding='UTF-8')
 lines = file.readlines()
@@ -11,6 +37,7 @@ for line in lines:
     for char in line:
         if char != ' ':
             mass_a.append(int(char))
+file.close()
 
 iterator = 1
 
@@ -25,38 +52,22 @@ while mass_a:
 
     # если первый элемент - минимум, то убераем его в другой стэк и сортируем дальше
     if ft_min(mass_a) == mass_a[0]:
-        file2.write('COMMAND pb' + '\n')
-        file2.write(f'MASSIVE_A: {mass_a}' + '\n')
-        file2.write(f'MASSIVE_B: {mass_b}' + '\n')
-        file2.write('=' * ft_len(f'MASSIVE_B: {mass_b}') + '\n')
-        print('COMMAND' + '\033[31m {}'.format('pb') + '\033[0m')
+        txt_output(file2, 'pb')
         mass_b.append(mass_a[0])
         mass_a = ft_slice(mass_a, 1, ft_len(mass_a))
-        print(f'MASSIVE_A: {mass_a}')
-        print(f'MASSIVE_B: {mass_b}')
-        print('=' * ft_len(f'MASSIVE_B: {mass_b}'))
+        console_output('pb')
     else:
         # находим рассстояние от концов
         # списка до концов списка и находим до куда ближе
         if minimal_index >= ft_len(mass_a) - minimal_index - 1:
             mass_a = rra(mass_a)
-            file2.write('COMMAND rra' + '\n')
-            file2.write(f'MASSIVE_A: {mass_a}' + '\n')
-            file2.write(f'MASSIVE_B: {mass_b}' + '\n')
-            file2.write('=' * ft_len(f'MASSIVE_B: {mass_b}') + '\n')
-            print('COMMAND' + '\033[31m {}'.format('rra') + '\033[0m')
-            print(f'MASSIVE_A: {mass_a}')
-            print(f'MASSIVE_B: {mass_b}')
-            print('=' * ft_len(f'MASSIVE_B: {mass_b}'))
+            txt_output(file2, 'rra')
+            console_output('rra')
         elif minimal_index <= ft_len(mass_a) - minimal_index - 1:
             mass_a = ra(mass_a)
-            file2.write('COMMAND ra' + '\n')
-            file2.write(f'MASSIVE_A: {mass_a}' + '\n')
-            file2.write(f'MASSIVE_B: {mass_b}' + '\n')
-            file2.write('=' * ft_len(f'MASSIVE_B: {mass_b}') + '\n')
-            print('COMMAND' + '\033[31m {}'.format('ra') + '\033[0m')
-            print(f'MASSIVE_A: {mass_a}')
-            print(f'MASSIVE_B: {mass_b}')
-            print('=' * ft_len(f'MASSIVE_B: {mass_b}'))
+            txt_output(file2, 'ra')
+            console_output('ra')
 
     iterator += 1
+
+file2.close()
