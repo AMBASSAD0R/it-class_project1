f0490cb8979aa6c4ef7aa92eedb54600fb9073c3
Tratut <trataty111@gmail.com>
2020-11-25 21:30:54 +0300

    Доработка визуализации Доработка output.txt Внешняя красота кода

--- sortic.py
+++ sortic.py
@@ -14,15 +14,22 @@ for line in lines:
 
 iterator = 1
 
+file2 = open('output.txt', mode='w')
+
 while mass_a:
 
     # индекс минимального элмента
     minimal_index = ft_find_lst(mass_a, ft_min(mass_a))
+    file2.write(f'Iteration: {iterator}' + '\n')
     print(f'Iteration: {iterator}')
 
     # если первый элемент - минимум, то убераем его в другой стэк и сортируем дальше
     if ft_min(mass_a) == mass_a[0]:
-        print("COMMAND" + "\033[31m {}" .format("pb") + "\033[0m")
+        file2.write('COMMAND pb' + '\n')
+        file2.write(f'MASSIVE_A: {mass_a}' + '\n')
+        file2.write(f'MASSIVE_B: {mass_b}' + '\n')
+        file2.write('=' * ft_len(f'MASSIVE_B: {mass_b}') + '\n')
+        print('COMMAND' + '\033[31m {}'.format('pb') + '\033[0m')
         mass_b.append(mass_a[0])
         mass_a = ft_slice(mass_a, 1, ft_len(mass_a))
         print(f'MASSIVE_A: {mass_a}')
@@ -33,26 +40,23 @@ while mass_a:
         # списка до концов списка и находим до куда ближе
         if minimal_index >= ft_len(mass_a) - minimal_index - 1:
             mass_a = rra(mass_a)
-            print("COMMAND" + "\033[31m {}".format("rra") + "\033[0m")
+            file2.write('COMMAND rra' + '\n')
+            file2.write(f'MASSIVE_A: {mass_a}' + '\n')
+            file2.write(f'MASSIVE_B: {mass_b}' + '\n')
+            file2.write('=' * ft_len(f'MASSIVE_B: {mass_b}') + '\n')
+            print('COMMAND' + '\033[31m {}'.format('rra') + '\033[0m')
             print(f'MASSIVE_A: {mass_a}')
             print(f'MASSIVE_B: {mass_b}')
             print('=' * ft_len(f'MASSIVE_B: {mass_b}'))
         elif minimal_index <= ft_len(mass_a) - minimal_index - 1:
             mass_a = ra(mass_a)
-            print("COMMAND" + "\033[31m {}" .format("ra") + "\033[0m")
+            file2.write('COMMAND ra' + '\n')
+            file2.write(f'MASSIVE_A: {mass_a}' + '\n')
+            file2.write(f'MASSIVE_B: {mass_b}' + '\n')
+            file2.write('=' * ft_len(f'MASSIVE_B: {mass_b}') + '\n')
+            print('COMMAND' + '\033[31m {}'.format('ra') + '\033[0m')
             print(f'MASSIVE_A: {mass_a}')
             print(f'MASSIVE_B: {mass_b}')
             print('=' * ft_len(f'MASSIVE_B: {mass_b}'))
 
     iterator += 1
-
-mass_a = mass_b
-
-result_string = ''
-for number in mass_a:
-    result_string += str(number) + ' '
-
-
-file = open('output.txt', mode='w')
-file.write(result_string)
-file.close()
